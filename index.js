33986796681623eff62e260a5bdf50a44a88aec2
Pranand Vathare <PranandV@winjittechnologiespvtltd.local>
2024-01-08 12:46:43 +0530

    updated swagger

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bookRoutes = require('./routes/bookRoutes');
 const { verifyToken } = require('./middlewares/authMiddleware');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger/Book-mgmt.json');
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 const config = require('./config'); // Import the configuration
 
@@ -26,9 +27,36 @@ mongoose.connect(config.mongodbUri,{
 
 
 // Routes
+app.get('/', (req, res) => {
+  const message = "System is running fine. Test APIs";
+  const htmlResponse = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Status Page</title>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #f4f4f4;
+          text-align: center;
+          margin-top: 100px;
+        }
+        .message {
+          font-size: 24px;
+          color: #008000; /* Green color */
+        }
+      </style>
+    </head>
+    <body>
+      <div class="message">${message}</div>
+    </body>
+    </html>
+  `;
+  res.send(htmlResponse);
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/books', verifyToken, bookRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument,{ customCssUrl: CSS_URL }));
 
 
 app.listen(PORT, () => {
